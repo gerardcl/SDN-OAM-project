b5feb61647a9fccef94ff4ed1386e3c60963c7dd
gerardcl <gerardcl@gmail.com>
2013-12-06 18:24:39 +0100

    all manager pojos, managers and resources

--- project/AppServer/src/main/java/dxat/appserver/manager/resources/OrgResource.java
+++ project/AppServer/src/main/java/dxat/appserver/manager/resources/OrgResource.java
@@ -1,5 +1,19 @@
 package dxat.appserver.manager.resources;
 
+import javax.ws.rs.Path;
+
+import dxat.appserver.manager.OrgManager;
+
+@Path("/manager")
 public class OrgResource {
+//	- orgCollection: OrgCollection
+//	+ getOrgIdNameList():HashMap<String, String>
+//	+ addOrg(TOrganization):TOrganization
+//	+ getOrg(String):TOrganization
+//	+ deleteOrg(String)
+//	+ updateOrg(TOrganization):TOrganization
+
+	private OrgManager orgManager = OrgManager.getInstance();
+
 
 }
