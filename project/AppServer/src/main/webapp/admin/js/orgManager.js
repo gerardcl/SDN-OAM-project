569a3c4a0d587b5c6fbf6beb4efa287eec98312d
Martí Boada <martiboada@MacBook-de-Marti-Boada.local>
2013-12-08 03:11:43 +0100

    first step with backbone. Org list hardcoded

--- project/AppServer/src/main/webapp/admin/js/orgManager.js
+++ project/AppServer/src/main/webapp/admin/js/orgManager.js
@@ -1,138 +1,139 @@
-(function($){
-	var Device = Backbone.Model.extend({
-		defaults: function(){
-			return {
-				id: ' ',
-				ip: ' ',
-				name: ' ',
-				description: ' '
-			}
-		}
+//SlimScroll sizes
+$(function(){
+  $('#OM-orgColumn').slimScroll({
+      height: '455px'
+  });
+  $('#OM-data').slimScroll({
+      height: '190px'
+  });
+  $('#OM-users').slimScroll({
+      height: '135px'
+  });
+  $('#OM-ap').slimScroll({
+      height: '135px'
+  });
+  $('#OM-flows').slimScroll({
+      height: '135px'
+  });
+  $('#OM-prgFlows').slimScroll({
+      height: '135px'
+  });
+  $('#OM-topo').slimScroll({
+      height: '230px'
+  });
+});
+
+//BACKBONE
+(function ($) {
+    var organizations = [{id:'1',name:'Ikea',cif:'111111111111',telephone:'+34 911111111',bankAccount:'1111111111111-01',isOAM:'false'},
+        {id:'2',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'false'},
+        {id:'3',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:'false'},
+        {id:'4',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:'false'},
+        {id:'5',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:'false'},
+        {id:'6',name:'Inditex',cif:'666666666666',telephone:'+34 966666666',bankAccount:'666666666666-06',isOAM:'false'},
+        {id:'7',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'false'},
+        {id:'8',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:'false'},
+        {id:'9',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:'false'},
+        {id:'10',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:'false'},
+        {id:'11',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'false'},
+        {id:'12',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:'false'},
+        {id:'13',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:'false'},
+        {id:'14',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:'false'},
+        {id:'15',name:'Inditex',cif:'666666666666',telephone:'+34 966666666',bankAccount:'666666666666-06',isOAM:'false'}];
+  
+    var Organization = Backbone.Model.extend({
+        defaults:{
+			id: ' ',
+			name: ' ',
+			cif: ' ',
+			telephone: ' ',
+			bankAccount: ' ',
+			isOAM: ' '
+        }
+    });
+
+    var Organizations = Backbone.Collection.extend({
+       model:Organization
+    });
+
+    var OrganizationView = Backbone.View.extend({
+        tagName:"div",
+        className:"organization-container",
+        template:$("#organization-template").html(),
+
+        render:function () {
+            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON object and returns html
+
+            this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. use $el to get access to jQuery html() function
+            return this;
+        }
+    });
+
+
+ var OrganizationsView = Backbone.View.extend({
+        el:$("#organizations"),
+
+        initialize:function(){
+            this.collection = new Organizations(organizations);
+            this.render();
+        },
+
+        render: function(){
+            var that = this;
+            _.each(this.collection.models, function(item){
+                that.renderOrganization(item);
+            }, this);
+        },
+
+        renderOrganization:function(item){
+            var organizationView = new OrganizationView({
+                model: item
+            });
+            this.$el.append(organizationView.render().el);
+        }
+    });
+
+    var organizationsView = new OrganizationsView();
+
+
+
+    var OrganizationDataView = Backbone.View.extend({
+        tagName:"div",
+        className:"orgData-container",
+        template:$("#organization-data-template").html(),
+
+        render:function () {
+            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON object and returns html
+
+            this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. use $el to get access to jQuery html() function
+            return this;
+        }
+    });
+
+
+	// Router
+	var AppRouter = Backbone.Router.extend({
+
+	    routes:{
+	        "":"list",
+	        "organizations/:id":"orgData"
+	    },
+
+	    list:function () {
+	        this.wineList = new WineCollection();
+	        this.wineListView = new WineListView({model:this.wineList});
+	        this.wineList.fetch();
+	        $('#organizations-container').html(this.wineListView.render().el);
+	    },
+
+	    orgData:function (id) {
+	        this.wine = this.wineList.get(id);
+	        this.wineView = new WineView({model:this.wine});
+	        $('#orgData-container').html(this.wineView.render().el);
+	    }
 	});
-	
-	var DevicesList = Backbone.Collection.extend({
-		model: Device
-	});
-	var numDevices = 0;
-	var devices = new DevicesList();
 
-	var DeviceView = Backbone.View.extend({
-		model: new Device(),
-		tagName: 'div',
-		events: {
-			'click .name': 'updateName',
-			'click .ip': 'updateIp',
-			'click .description': 'updateDesc',
-			'click .delete': 'delete',
-			'blur .name': 'close',
-			'blur .ip': 'close',
-			'blur .description': 'close',
-			'keypress .name': 'onEnterUpdate',
-			'keypress .ip': 'onEnterUpdate',
-			'keypress .description': 'onEnterUpdate'
-		},
-		initialize: function(){
-			this.template = _.template($('#device-template').html()); //using underscore template
-		},
-		updateName: function(ev){
-			ev.preventDefault();
-			this.$('.name').attr('contenteditable',true).focus();
-		},
-		updateIp: function(ev){
-			ev.preventDefault();
-			this.$('.ip').attr('contenteditable',true).focus();
-		},
-		updateDesc: function(ev){
-			ev.preventDefault();
-			this.$('.description').attr('contenteditable',true).focus();
-		},
-		close: function(ev){
-			var name = this.$('.name').text();
-			var ip = this.$('.ip').text();
-			var description = this.$('.description').text();
-			this.model.set('name', name);
-			
-			var self=this;
+	var app = new AppRouter();
+	Backbone.history.start();
 
-			console.log("ip: "+ip);
-
-
-			this.model.set('ip', ip);
-/*
-			//Testing ip update pattern
-			var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/; 		
-			
-			if(ip.match(ipformat)){
-				this.model.set('ip', ip);  
-				}
-			
-			else{
-				var ip2=prompt("Please enter a valid IP","192.168.1.1");
-				console.log("ip2 promt: "+ip2);
-				if(ip2.match(ipformat)){
-					self.model.set('ip', ip2);
-					ip2=null;
-				}
-			}
-
-*/
-			this.model.set('description', description);
-			this.$('.name').removeAttr('contenteditable');
-			this.$('.ip').removeAttr('contenteditable');
-			this.$('.description').removeAttr('contenteditable');
-		},
-		onEnterUpdate: function(ev){
-			var self = this;
-			if(ev.keyCode==13){
-				this.close();
-				_.delay(function(){ self.$('.name').blur()},100);
-				_.delay(function(){ self.$('.ip').blur()},100);
-				_.delay(function(){ self.$('.description').blur()},100);
-			}
-		},
-		delete: function(ev){
-			ev.preventDefault();
-			devices.remove(this.model);
-		},
-		render: function(){
-			this.$el.html(this.template(this.model.toJSON()));
-			return this;
-		}
-	});
-	
-	var DevicesView = Backbone.View.extend({
-		model: devices,
-		el: $('#devices-container'),
-		initialize: function(){
-			this.model.on('add', this.render, this);
-			this.model.on('remove', this.render,this);
-		},					
-		render: function(){
-			var self = this;
-			self.$el.html('');
-			_.each(this.model.toArray(),function(device,i){
-				self.$el.append((new DeviceView({model: device})).render().$el);
-			});
-			return this;
-		}
-		
-	});
-	
-	$(document).ready(function(){
-		$('#add-device').submit(function(ev){
-			var device = new Device({id:++numDevices,name:$('#device-name').val(),ip:$('#device-ip').val(),description:$('#device-description').val()});
-			devices.add(device);
-			//var device2 = new Device({id:'RT-2',name:'Router 2',ip:'192.168.1.12',description:'second router'});
-			//var device3 = new Device({id:'RT-3',name:'Router 3',ip:'192.168.1.13',description:'third router'});
-			//console.log(device.get('description'));
-			console.log(devices.toJSON());
-
-
-			return false;	
-		});
-		
-		var appView = new DevicesView();
-	});
-	
-	
 })(jQuery);
