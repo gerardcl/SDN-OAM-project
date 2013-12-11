faaf699ad1e0660fbd2f3ede8b5e0498a97dd9ba
Martí Boada <martiboada@MacBook-de-Marti-Boada.local>
2013-12-11 18:13:25 +0100

    bacbone organization data loaded

--- project/AppServer/src/main/webapp/admin/js/orgManager.js
+++ project/AppServer/src/main/webapp/admin/js/orgManager.js
@@ -26,6 +26,8 @@ $(function(){
 //BACKBONE
 (function () {
   
+
+  //MODELS
     var Organization = Backbone.Model.extend({
         defaults:{
 			id: ' ',
@@ -76,27 +78,28 @@ $(function(){
         }
     });
 
+
+//COLLECTIONS HARDCODED
     var Organizations = Backbone.Collection.extend({
-       model:Organization,
-       url: '/organizations'
+       model:Organization
     });
     
-    //ORGANIZATIONS LIST HARDCODED
-    var organizations = [{id:'1',name:'Ikea',cif:'111111111111',telephone:'+34 911111111',bankAccount:'1111111111111-01',isOAM:'false'},
-        {id:'2',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'false'},
-        {id:'3',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:'false'},
-        {id:'4',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:'false'},
-        {id:'5',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:'false'},
-        {id:'6',name:'Inditex',cif:'666666666666',telephone:'+34 966666666',bankAccount:'666666666666-06',isOAM:'false'},
-        {id:'7',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'false'},
-        {id:'8',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:'false'},
-        {id:'9',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:'false'},
-        {id:'10',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:'false'},
-        {id:'11',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'false'},
-        {id:'12',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:'false'},
-        {id:'13',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:'false'},
-        {id:'14',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:'false'},
-        {id:'15',name:'Inditex',cif:'666666666666',telephone:'+34 966666666',bankAccount:'666666666666-06',isOAM:'false'}];
+
+    var organizations = [{id:'1',name:'Ikea',cif:'111111111111',telephone:'+34 911111111',bankAccount:'1111111111111-01',isOAM:'checked'},
+        {id:'2',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:'no'},
+        {id:'3',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:false},
+        {id:'4',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:false},
+        {id:'5',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:false},
+        {id:'6',name:'Inditex',cif:'666666666666',telephone:'+34 966666666',bankAccount:'666666666666-06',isOAM:false},
+        {id:'7',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:false},
+        {id:'8',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:false},
+        {id:'9',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:false},
+        {id:'10',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:false},
+        {id:'11',name:'Danone',cif:'222222222222',telephone:'+34 922222222',bankAccount:'22222222222222-02',isOAM:false},
+        {id:'12',name:'National Instruments',cif:'333333333333',telephone:'+34 933333333',bankAccount:'333333333333-03',isOAM:false},
+        {id:'13',name:'Kraft',cif:'444444444444',telephone:'+34 944444444',bankAccount:'444444444444-04',isOAM:false},
+        {id:'14',name:'Nestle',cif:'555555555555',telephone:'+34 955555555',bankAccount:'555555555555-05',isOAM:false},
+        {id:'15',name:'Inditex',cif:'666666666666',telephone:'+34 966666666',bankAccount:'666666666666-06',isOAM:false}];
 
     var Flows = Backbone.Collection.extend({
        model:Flow
@@ -133,45 +136,28 @@ $(function(){
 
             this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. use $el to get access to jQuery html() function
             return this;
-        }
+        },
     });
 
 
-    var OrgUsersView = Backbone.View.extend({
-        tagName:"div",
-        className:"orgUsers-container",
-        template:$("#orgUsers-template").html(),
-
-        render:function () {
-            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON object and returns html
-
-            this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. use $el to get access to jQuery html() function
-            return this;
-        }
-    });
 
 
  var OrganizationsView = Backbone.View.extend({
         el:$("#organizations"),
-        events: {
-              'click .name': 'orgSelected'
-            },
 
         initialize:function(){
             this.collection = new Organizations(organizations);
             this.render();
+            //console.log(this.collection.toJSON());
         },
 
         render: function(){
+          console.log('ENTRA AL RENDER (OrganizationsView)');
+          console.log(this.collection.toJSON());
             var that = this;
             _.each(this.collection.models, function(item){
                 that.renderOrganization(item);
             }, this);
-        },
-
-        orgSelected: function(ev){
-          ev.preventDefault();
-          this.$('.name').attr('contenteditable',true).focus();
         },
 
         renderOrganization:function(item){
@@ -181,8 +167,6 @@ $(function(){
             this.$el.append(organizationView.render().el);
         }
     });
-
-    var organizationsView = new OrganizationsView();
 
 
 
@@ -197,27 +181,44 @@ $(function(){
         }
     });
 
-    // Router
-    var AppRouter = Backbone.Router.extend({
+    var OrgNameView = Backbone.View.extend({
+        tagName:"h3",
+        className:"organization-name-container",
+        template:_.template($("#organization-name-template").html()),
 
-        routes:{
-            "":"list",
-            "organizations/:id":"organizationDetails"
-        },
-
-        list:function () {
-            this.organizationList = new Organizations();
-            this.organizationListView = new OrganizationsView({model:this.organizationList});
-            this.organizationList.fetch();
-            $('#organizations-container').html(this.organizationListView.render().el);
-        },
-
-        organizationDetails:function (id) {
-            this.organization = this.organizationList.get(id);
-            this.organizationView = new OrganizationView({model:this.organization});
-            $('#orgData-container').html(this.organizationView.render().el);
+        render:function () {
+            var tmpl = _.template(this.template); //tmpl is a function that takes a JSON object and returns html
+            this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. use $el to get access to jQuery html() function
+            return this;
         }
     });
+
+// Router
+var AppRouter = Backbone.Router.extend({
+
+    routes:{
+        "":"list",
+        "orgData/:id":"orgData"
+    },
+
+    list:function () {
+        this.collection = new Organizations(organizations);
+        this.organizationsView = new OrganizationsView({model:this.collection});
+        this.collection.fetch();
+        $('#organizations-container').html(this.organizationsView.render().el);
+        $('#organization-name-container').html(this.organizationView.render().el);
+    },
+
+    orgData:function (id) {
+        this.organization = this.collection.get(id);
+        this.organizationView = new OrgDataView({model:this.organization});
+        $('#orgData-container').html(this.organizationView.render().el);
+
+        this.organization = this.collection.get(name);
+        this.orgNameView = new OrgNameView({model:this.organization});
+        $('#organization-name-container').html(this.orgNameView.render().el);
+    }
+});
 
 var app = new AppRouter();
 Backbone.history.start();
