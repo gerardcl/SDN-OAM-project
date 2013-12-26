6ec67461740dfd532b0d81fec576a3e41e05a0bc
gerardcl <gerardcl@gmail.com>
2013-12-26 23:43:07 +0100

    read class reading for orgmanager initalization, now torgs and orgs withou users terminals and flows

--- project/AppServer/src/main/java/dxat/appserver/manager/OrgManager.java
+++ project/AppServer/src/main/java/dxat/appserver/manager/OrgManager.java
@@ -3,11 +3,11 @@ package dxat.appserver.manager;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
 
 import dxat.appserver.manager.database.Create;
+import dxat.appserver.manager.database.Read;
 import dxat.appserver.manager.exceptions.FlowAlreadyExistsException;
 import dxat.appserver.manager.exceptions.OrgAlreadyExistsException;
 import dxat.appserver.manager.exceptions.OrgNotFoundException;
@@ -31,154 +31,12 @@ public class OrgManager {
 	private HashMap<String, OrgSession> sessions;
 	private HashMap<String, OrgTerminal> terminals;
 	private Create dbcreate;
+	private Read dbread;
+	private boolean dbExists;
 
 	private OrgManager(){
-		dbcreate = new Create();
-		orgs = new HashMap<String, Org>();
-		torgs = new HashMap<String, TOrg>();
-		flows = new HashMap<String, OrgFlow>();
-		users = new HashMap<String, OrgUser>();
-		terminals = new HashMap<String, OrgTerminal>();
-		sessions = new HashMap<String, OrgSession>();
-		
-		/*some test init*/
-		int maxorgs = 10;
-		int i = 0;
-		int j = 0;
-		for(j = 1; j < maxorgs; j++){
-			Org org = new Org();
-			String id = "orgId";
-			String name = "orgName";
-			id += Integer.toString(j);
-			name += Integer.toString(j);
-			org.setIdentifier(id);
-			org.setName(name);
-			org.setNIF("C-123456"+Integer.toString(i)+Integer.toString(j));
-			org.setTelephone("689404"+Integer.toString(i)+Integer.toString(j));
-			org.setBankAccount("2013-1234-5678-2345234"+Integer.toString(j)+Integer.toString(i));
-			org.setOAM(j%(maxorgs-1)==0?true:false);
-			HashMap<String, OrgFlow> tempFlows;
-			HashMap<String, OrgUser> tempUsers;
-			HashMap<String, OrgTerminal> tempTerminals;			
-			TOrg torg = new TOrg();
-			torg.setBankAccount(org.getBankAccount());
-			torg.setIdentifier(org.getIdentifier());
-			torg.setName(org.getName());
-			torg.setNIF(org.getNIF());
-			torg.setOAM(org.isOAM());
-			torg.setTelephone(org.getTelephone());
-			org.setTorg(torg);
-			
-			try {
-				dbcreate.createOrg(torg);
-			} catch (OrgAlreadyExistsException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			tempFlows = new HashMap<String, OrgFlow>();
-			for(i=0; i<10; i++){
-				OrgFlow flow = new OrgFlow();
-				String fid = "flouu";
-				String fname = "namee";
-				String orgTS = "src";
-				String orgTD = "dst";
-				fid += Integer.toString(j);
-				fid += Integer.toString(i);
-				fname += Integer.toString(j);
-				fname += Integer.toString(i);
-				orgTS += Integer.toString(j);
-				orgTS += Integer.toString(i);
-				orgTD += Integer.toString(j);
-				orgTD += Integer.toString(i);
-				flow.setIdentifier(fid);
-				flow.setBandwidth(5000000);
-				flow.setDstPort(5000);
-				flow.setSrcPort(6000);
-				flow.setProtocol("TCP");
-				flow.setName(fname);
-				flow.setQos(2000);
-				flow.setActive(i%3==0?true:false);
-				flow.setDstOTidentifier(orgTD);
-				flow.setSrcOTidentifier(orgTS);
-				tempFlows.put(flow.identifier, flow);
-				flows.put(flow.identifier, flow);
-				try {
-					dbcreate.createFlow(flow, org.getIdentifier());
-				} catch (FlowAlreadyExistsException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (OrgNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			org.setFlows(tempFlows);
-			
-			tempUsers = new HashMap<String, OrgUser>();
-			for(i=0; i<10; i++){
-				OrgUser user = new OrgUser();
-				String uid = "useer";
-				String uname = "namee";
-				uid += Integer.toString(j);
-				uid += Integer.toString(i);
-				uname += Integer.toString(j);
-				uname += Integer.toString(i);
-				user.setIdentifier(uid);
-				user.setName(uname);
-				user.setEmail(uid+"@"+org.getName()+".com");
-				user.setTelephone(654321000+i*100+j*10+i+j);
-				user.setPassword(Integer.toString(j)+"dxat"+Integer.toString(i));
-				user.setAdmin(j%(maxorgs-1)==0?true:false);
-				user.setActive(i%3==0?true:false);
-				tempUsers.put(user.identifier, user);
-				users.put(user.identifier, user);
-				try {
-					dbcreate.createUser(user, org.getIdentifier());
-				} catch (OrgNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UserAlreadyExistsException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			org.setUsers(tempUsers);
-			
-			tempTerminals = new HashMap<String, OrgTerminal>();
-			for(i=0; i<10; i++){
-				OrgTerminal terminal = new OrgTerminal();
-				String tid = "terminall";
-				String tname = "namee";
-				tid += Integer.toString(j);
-				tid += Integer.toString(i);
-				tname += Integer.toString(j);
-				tname += Integer.toString(i);
-				terminal.setIdentifier(tid);
-				terminal.setHostName(tname);
-				terminal.setDescription("terminal host for ");
-				terminal.setDescription(terminal.getDescription() + org.getName());
-				terminal.setIfaceSpeed(i*10000);
-				terminal.setIpAddress("192.168."+Integer.toString(i)+"."+Integer.toString(j)+"0");
-				terminal.setMac("DD:XX:AA:TT:"+Integer.toString(j)+"B:"+Integer.toString(i)+"C");
-				terminal.setActive(i%2==0?true:false);
-				tempTerminals.put(terminal.identifier, terminal);
-				terminals.put(terminal.identifier, terminal);
-				try {
-					dbcreate.createTerminal(terminal, org.getIdentifier());
-				} catch (TerminalAlreadyExistsException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (OrgNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			org.setTerminals(tempTerminals);
-			
-			orgs.put(org.getIdentifier(), org);
-			torgs.put(torg.getIdentifier(), torg);
-		}
+		dbExists = true;
+		initOrgManager();
 	}
 
 	public HashMap<String, OrgFlow> getFlows() {
@@ -297,5 +155,154 @@ public class OrgManager {
 	public List<Org> getOrgs(){
 		//TODO
 		return null;
+	}
+	
+	private void initOrgManager(){
+		if(dbExists){	//CREATE ORGS FROM EXISTING DB
+			dbread = new Read();
+			orgs = dbread.getDBOrgs();
+			torgs = dbread.getDBTOrgs();
+			flows = dbread.getDBFlows();
+			users = dbread.getDBUsers();
+			terminals = dbread.getDBTerminals();
+			sessions = new HashMap<String, OrgSession>();
+			
+		}else{ 			//CREATE DB FOR THE FIRST TIME 
+			dbcreate = new Create();
+			orgs = new HashMap<String, Org>();
+			torgs = new HashMap<String, TOrg>();
+			flows = new HashMap<String, OrgFlow>();
+			users = new HashMap<String, OrgUser>();
+			terminals = new HashMap<String, OrgTerminal>();
+			sessions = new HashMap<String, OrgSession>();
+			
+			/*orgs test fake init*/
+			int maxorgs = 10;
+			int i = 0;
+			int j = 0;
+			for(j = 1; j < maxorgs; j++){
+				Org org = new Org();
+				String id = "orgId";
+				String name = "orgName";
+				id += Integer.toString(j);
+				name += Integer.toString(j);
+				org.setIdentifier(id);
+				org.setName(name);
+				org.setNIF("C-123456"+Integer.toString(i)+Integer.toString(j));
+				org.setTelephone("689404"+Integer.toString(i)+Integer.toString(j));
+				org.setBankAccount("2013-1234-5678-2345234"+Integer.toString(j)+Integer.toString(i));
+				org.setOAM(j%(maxorgs-1)==0?true:false);
+				HashMap<String, OrgFlow> tempFlows;
+				HashMap<String, OrgUser> tempUsers;
+				HashMap<String, OrgTerminal> tempTerminals;			
+				TOrg torg = new TOrg();
+				torg.setBankAccount(org.getBankAccount());
+				torg.setIdentifier(org.getIdentifier());
+				torg.setName(org.getName());
+				torg.setNIF(org.getNIF());
+				torg.setOAM(org.isOAM());
+				torg.setTelephone(org.getTelephone());
+				org.setTorg(torg);
+				try {
+					dbcreate.createOrg(torg);
+				} catch (OrgAlreadyExistsException e) {
+					e.printStackTrace();
+				}
+				tempFlows = new HashMap<String, OrgFlow>();
+				for(i=0; i<10; i++){
+					OrgFlow flow = new OrgFlow();
+					String fid = "flouu";
+					String fname = "namee";
+					String orgTS = "src";
+					String orgTD = "dst";
+					fid += Integer.toString(j);
+					fid += Integer.toString(i);
+					fname += Integer.toString(j);
+					fname += Integer.toString(i);
+					orgTS += Integer.toString(j);
+					orgTS += Integer.toString(i);
+					orgTD += Integer.toString(j);
+					orgTD += Integer.toString(i);
+					flow.setIdentifier(fid);
+					flow.setBandwidth(5000000);
+					flow.setDstPort(5000);
+					flow.setSrcPort(6000);
+					flow.setProtocol("TCP");
+					flow.setName(fname);
+					flow.setQos(2000);
+					flow.setActive(i%3==0?true:false);
+					flow.setDstOTidentifier(orgTD);
+					flow.setSrcOTidentifier(orgTS);
+					tempFlows.put(flow.identifier, flow);
+					flows.put(flow.identifier, flow);
+					try {
+						dbcreate.createFlow(flow, org.getIdentifier());
+					} catch (FlowAlreadyExistsException e) {
+						e.printStackTrace();
+					} catch (OrgNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+				org.setFlows(tempFlows);
+				tempUsers = new HashMap<String, OrgUser>();
+				for(i=0; i<10; i++){
+					OrgUser user = new OrgUser();
+					String uid = "useer";
+					String uname = "namee";
+					uid += Integer.toString(j);
+					uid += Integer.toString(i);
+					uname += Integer.toString(j);
+					uname += Integer.toString(i);
+					user.setIdentifier(uid);
+					user.setName(uname);
+					user.setEmail(uid+"@"+org.getName()+".com");
+					user.setTelephone(654321000+i*100+j*10+i+j);
+					user.setPassword(Integer.toString(j)+"dxat"+Integer.toString(i));
+					user.setAdmin(j%(maxorgs-1)==0?true:false);
+					user.setActive(i%3==0?true:false);
+					tempUsers.put(user.identifier, user);
+					users.put(user.identifier, user);
+					try {
+						dbcreate.createUser(user, org.getIdentifier());
+					} catch (OrgNotFoundException e) {
+						e.printStackTrace();
+					} catch (UserAlreadyExistsException e) {
+						e.printStackTrace();
+					}
+				}
+				org.setUsers(tempUsers);
+				tempTerminals = new HashMap<String, OrgTerminal>();
+				for(i=0; i<10; i++){
+					OrgTerminal terminal = new OrgTerminal();
+					String tid = "terminall";
+					String tname = "namee";
+					tid += Integer.toString(j);
+					tid += Integer.toString(i);
+					tname += Integer.toString(j);
+					tname += Integer.toString(i);
+					terminal.setIdentifier(tid);
+					terminal.setHostName(tname);
+					terminal.setDescription("terminal host for ");
+					terminal.setDescription(terminal.getDescription() + org.getName());
+					terminal.setIfaceSpeed(i*10000);
+					terminal.setIpAddress("192.168."+Integer.toString(i)+"."+Integer.toString(j)+"0");
+					terminal.setMac("DD:XX:AA:TT:"+Integer.toString(j)+"B:"+Integer.toString(i)+"C");
+					terminal.setActive(i%2==0?true:false);
+					tempTerminals.put(terminal.identifier, terminal);
+					terminals.put(terminal.identifier, terminal);
+					try {
+						dbcreate.createTerminal(terminal, org.getIdentifier());
+					} catch (TerminalAlreadyExistsException e) {
+						e.printStackTrace();
+					} catch (OrgNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+				org.setTerminals(tempTerminals);
+				orgs.put(org.getIdentifier(), org);
+				torgs.put(torg.getIdentifier(), torg);
+			}
+		}
+		
 	}
 }
--- project/AppServer/src/main/java/dxat/appserver/manager/database/Read.java
+++ project/AppServer/src/main/java/dxat/appserver/manager/database/Read.java
@@ -1,14 +1,30 @@
 package dxat.appserver.manager.database;
 
+import java.util.HashMap;
+
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import dxat.appserver.manager.pojos.Org;
+import dxat.appserver.manager.pojos.OrgFlow;
+import dxat.appserver.manager.pojos.OrgTerminal;
+import dxat.appserver.manager.pojos.OrgUser;
+import dxat.appserver.manager.pojos.TOrg;
 
 public class Read {
-	
-	
+
+	private static final String OCOLLECTION   = "ORG_COLLECTION";
+
+	DBAccess dbaccess = new DBAccess();
+	DB db = dbaccess.setDb();
+
 	public boolean existsOrg(DB db, String id){
 		return true;
 	}
-	
+
 	public boolean existsUser(){
 		return true;
 	}
@@ -17,5 +33,104 @@ public class Read {
 	}
 	public boolean existsFlow(){
 		return true;
+	}
+
+	public boolean existsOrg(DBCollection collection, String id) {
+		BasicDBObject query = new BasicDBObject("identifier", id);
+		DBCursor cursor 	= collection.find(query);
+		if(cursor.count()!=0) return true;
+		else return false;
+
+	}
+	public boolean existsElement(DBCollection collection, String idOrg, String id, String type){
+		BasicDBObject query = new BasicDBObject("identifier", idOrg);
+		query.put("users.identifier", id);
+		DBCursor cursor = collection.find(query);
+		if(cursor.count()!=0) return true;
+		else return false;
+	}
+
+	public HashMap<String, Org> getDBOrgs() {
+		HashMap<String, Org> orgs = new HashMap<String, Org>();
+		DBCollection collection = db.getCollection(OCOLLECTION);
+		DBCursor cursor;
+		
+		cursor = collection.find();
+		while(cursor.hasNext()) {
+			Org org = new Org();
+			DBObject o = cursor.next();
+			String identifier = (String) o.get("identifier") ; 
+			String name = (String) o.get("name") ; 
+			String NIF = (String) o.get("NIF") ; 
+			String telephone = (String) o.get("telephone") ; 
+			String bankAccount = (String) o.get("bankAccount") ; 
+			String isOAM_s = (String) o.get("isOAM").toString() ;
+			boolean isOAM = (isOAM_s.equals("true")? true : false);
+			TOrg torg = new TOrg();
+			torg.setIdentifier(identifier);
+			torg.setName(name);
+			torg.setNIF(NIF);
+			torg.setTelephone(telephone);
+			torg.setBankAccount(bankAccount);
+			torg.setOAM(isOAM);
+			//users
+			//flows
+			//terminals
+			
+			org.setIdentifier(identifier);
+			org.setName(name);
+			org.setNIF(NIF);
+			org.setTelephone(telephone);
+			org.setBankAccount(bankAccount);
+			org.setOAM(isOAM);
+			org.setTorg(torg);
+			
+			orgs.put(identifier, org);
+		}
+		return orgs;
+	}
+
+	public HashMap<String, TOrg> getDBTOrgs() {
+		HashMap<String, TOrg> torgs = new HashMap<String, TOrg>();
+		DBCollection collection = db.getCollection(OCOLLECTION);
+		DBCursor cursor;
+		
+		cursor = collection.find();
+		while(cursor.hasNext()) {
+			TOrg torg = new TOrg();
+			DBObject o = cursor.next();
+			String identifier = (String) o.get("identifier") ; 
+			String name = (String) o.get("name") ; 
+			String NIF = (String) o.get("NIF") ; 
+			String telephone = (String) o.get("telephone") ; 
+			String bankAccount = (String) o.get("bankAccount") ; 
+			String isOAM_s = (String) o.get("isOAM").toString() ;
+			boolean isOAM = (isOAM_s.equals("true")? true : false);
+			
+			torg.setIdentifier(identifier);
+			torg.setName(name);
+			torg.setNIF(NIF);
+			torg.setTelephone(telephone);
+			torg.setBankAccount(bankAccount);
+			torg.setOAM(isOAM);
+			
+			torgs.put(identifier, torg);
+		}
+		return torgs;
+	}
+
+	public HashMap<String, OrgFlow> getDBFlows() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public HashMap<String, OrgUser> getDBUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public HashMap<String, OrgTerminal> getDBTerminals() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
