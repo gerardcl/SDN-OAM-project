d63423df4f2a49eea88c3d41bb7aef9f80f723eb
gerardcl <gerardcl@gmail.com>
2013-12-27 00:45:05 +0100

    get users, flows and terminals indications to hashmaps Ⓐ

--- project/AppServer/src/main/java/dxat/appserver/manager/database/Read.java
+++ project/AppServer/src/main/java/dxat/appserver/manager/database/Read.java
@@ -1,5 +1,6 @@
 package dxat.appserver.manager.database;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 
 import com.mongodb.BasicDBObject;
@@ -54,7 +55,7 @@ public class Read {
 		HashMap<String, Org> orgs = new HashMap<String, Org>();
 		DBCollection collection = db.getCollection(OCOLLECTION);
 		DBCursor cursor;
-		
+
 		cursor = collection.find();
 		while(cursor.hasNext()) {
 			Org org = new Org();
@@ -74,9 +75,12 @@ public class Read {
 			torg.setBankAccount(bankAccount);
 			torg.setOAM(isOAM);
 			//users
+			//TODO cargar hashmap users de dentro de org <-- getDBOrgUser(orgId/identifier) 
 			//flows
+			//TODO cargar hashmap flows de dentro de org <-- getDBOrgFlows(orgId/identifier) 
 			//terminals
-			
+			//TODO cargar hashmap terminals de dentro de org <-- getDBOrgTerminals(ororgId/identifiergId) 
+
 			org.setIdentifier(identifier);
 			org.setName(name);
 			org.setNIF(NIF);
@@ -84,7 +88,7 @@ public class Read {
 			org.setBankAccount(bankAccount);
 			org.setOAM(isOAM);
 			org.setTorg(torg);
-			
+
 			orgs.put(identifier, org);
 		}
 		return orgs;
@@ -94,7 +98,7 @@ public class Read {
 		HashMap<String, TOrg> torgs = new HashMap<String, TOrg>();
 		DBCollection collection = db.getCollection(OCOLLECTION);
 		DBCursor cursor;
-		
+
 		cursor = collection.find();
 		while(cursor.hasNext()) {
 			TOrg torg = new TOrg();
@@ -106,29 +110,54 @@ public class Read {
 			String bankAccount = (String) o.get("bankAccount") ; 
 			String isOAM_s = (String) o.get("isOAM").toString() ;
 			boolean isOAM = (isOAM_s.equals("true")? true : false);
-			
+
 			torg.setIdentifier(identifier);
 			torg.setName(name);
 			torg.setNIF(NIF);
 			torg.setTelephone(telephone);
 			torg.setBankAccount(bankAccount);
 			torg.setOAM(isOAM);
-			
+
 			torgs.put(identifier, torg);
 		}
 		return torgs;
 	}
+	//RETURN ORG USERS
+	public HashMap<String, OrgUser> getDBOrgUser(String idOrg) {
+		return null;
+	}
+	//RETURN ALL USERS
+	public HashMap<String, OrgUser> getDBUsers() {
+		HashMap<String, OrgFlow> usersHM = null;
+		DBCollection collection = db.getCollection(OCOLLECTION);
+		DBCursor cursor;
 
+		cursor = collection.find();
+		while(cursor.hasNext()) {
+			DBObject o = cursor.next();
+			Object users = o.get("users");
+
+			//users[i] 
+			System.out.println("GETTING USERS");
+			System.out.println(users);
+			//TODO RECORRER EL ARRAY USERS (mira consola cuando haces llamada a la web
+			//para que veas que efectivamente users és una array (confirmado): 
+			//http://localhost:8080/AppServer/#/adminOrgs
+			//para tener una db inicializada como hacemos tienes primero que poner el
+			//booleano en OrgManager a true (dbExists)
+			//luego cuando ya tengas la db montada (comprueba con robomongo)
+			//podras volverla a false y a jugar
+			//NOTA: esta implementació serà la clave para getDBOrgUser luego
+
+
+
+		}
+		return null;
+	}
 	public HashMap<String, OrgFlow> getDBFlows() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public HashMap<String, OrgUser> getDBUsers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	public HashMap<String, OrgTerminal> getDBTerminals() {
 		// TODO Auto-generated method stub
 		return null;
