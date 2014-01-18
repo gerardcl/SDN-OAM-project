4114c4edc59aebc9d626a54be6fd91892fb2a44f
GerardCL <gerardcl@gmail.com>
2014-01-18 19:00:13 +0100

    weather map 1rst step

--- project/AppServer/src/main/webapp/js/statusOverview.js
+++ project/AppServer/src/main/webapp/js/statusOverview.js
@@ -12,16 +12,21 @@ function initStatusOverview(){
 }
 
 //TOPOLOGY GRAPH
-function createTopologyGraph(){
-	var switchesURL = "/AppServer/webapi/topology/switches";
-	var terminalsURL = "/AppServer/webapi/topology/terminals";
-	var linksURL = "/AppServer/webapi/topology/links";
-	var dataSwitches;
-	var dataTerminals;
-	var dataLinks;
-	var nodes = [];
-	var links = [];
+var switchesURL = "/AppServer/webapi/topology/switches";
+var terminalsURL = "/AppServer/webapi/topology/terminals";
+var linksURL = "/AppServer/webapi/topology/links";
+var dataSwitches;
+var dataTerminals;
+var dataLinks;
+var nodes = [];
+var links = [];
+var refreshIntervalTWM;
 
+function stopTopoWeatherMapRefresh(){
+	clearInterval(refreshIntervalTWM);
+}
+
+function getTopoWeatherMap(){
 	$.ajaxSetup({
 		async : false
 	}); //execute synchronously
@@ -75,26 +80,150 @@ function createTopologyGraph(){
 		}
 	}
 	nodes = dataSwitches.switches;
-	var rawLinks = dataLinks.links;
+	//var rawLinks = dataLinks.links;
 	var src,trg;
 
-	for (var i=0;i<rawLinks.length;i++){
+	var new_obj = {"colorTx":0, "colorRx":0};
+	dataLinks.links.push( new_obj );
+	dataTerminals.terminals.push( new_obj );
+
+	for (var i=0; i<dataLinks.links.length; i++){
+		//alert(i);
+		//Dummy request to..
+		var reqUriRx ="/AppServer/webapi/statistics/port/"+dataLinks.links[i].dstPortId+"/receiveBytes/AVERAGE/minute";
+		//alert (reqUriRx);
+		//Obtained DUMMYajax response
+		var response1;// = {"idObject":"00:01:d4:ca:6d:c4:44:1e:3","parameter":"receiveBytes","timeAxxis":[1389907545,1389907546,1389907547,1389907548,1389907549,1389907550,1389907551,1389907552,1389907553,1389907554,1389907555,1389907556,1389907557,1389907558,1389907559,1389907560,1389907561,1389907562,1389907563,1389907564,1389907565,1389907566,1389907567,1389907568,1389907569,1389907570,1389907571,1389907572,1389907573,1389907574,1389907575,1389907576,1389907577,1389907578,1389907579,1389907580,1389907581,1389907582,1389907583,1389907584,1389907585,1389907586,1389907587,1389907588,1389907589,1389907590,1389907591,1389907592,1389907593,1389907594,1389907595,1389907596,1389907597,1389907598,1389907599,1389907600,1389907601,1389907602,1389907603,1389907604],"valueAxxis":[0.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,695.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]};
+		$.ajax({
+			type: "GET",
+			url: reqUriRx,
+			//contentType: 'application/json',
+			//datatype: "application/vmd.dxat.appserver.topology.switches.collection+json",
+//			headers: {
+//			Accept : "application/vmd.dxat.appserver.topology.switches.collection+json"
+//			},
+			success : function(result) {
+				response1 = result;
+			}
+		});
+		
+		//Dummy request to...
+		var reqUriTx ="/AppServer/webapi/statistics/port/"+dataLinks.links[i].srcPortId+"/transmitBytes/AVERAGE/minute";
+		//alert (reqUriTx);
+		//Obtained DUMMYajax response
+		var response2;//={"idObject":"00:01:d4:ca:6d:c4:44:1e:3","parameter":"transmitBytes","timeAxxis":[1389908038,1389908039,1389908040,1389908041,1389908042,1389908043,1389908044,1389908045,1389908046,1389908047,1389908048,1389908049,1389908050,1389908051,1389908052,1389908053,1389908054,1389908055,1389908056,1389908057,1389908058,1389908059,1389908060,1389908061,1389908062,1389908063,1389908064,1389908065,1389908066,1389908067,1389908068,1389908069,1389908070,1389908071,1389908072,1389908073,1389908074,1389908075,1389908076,1389908077,1389908078,1389908079,1389908080,1389908081,1389908082,1389908083,1389908084,1389908085,1389908086,1389908087,1389908088,1389908089,1389908090,1389908091,1389908092,1389908093,1389908094,1389908095,1389908096,1389908097],"valueAxxis":[12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0]};
+		$.ajax({
+			type: "GET",
+			url: reqUriTx,
+			//contentType: 'application/json',
+			//datatype: "application/vmd.dxat.appserver.topology.switches.collection+json",
+//			headers: {
+//			Accept : "application/vmd.dxat.appserver.topology.switches.collection+json"
+//			},
+			success : function(result) {
+				response2 = result;
+			}
+		});
+		//Transmitted bytes
+		//Recieved bytes
+		var sumRx=0;
+		var sumTx=0;
+
+		for (var j=0; j<response1.valueAxxis.length;j++){
+			sumRx+=response1.valueAxxis[j];
+			sumTx+=response2.valueAxxis[j];
+		}
+
+		sumRx = ((sumRx/60)*8/1000000).toFixed(2);
+		sumTx = ((sumTx/60)*8/1000000).toFixed(2);
+		//alert("REC: "+sumRx+" TRANSM: "+sumTx);
+		var colorRx = getGreenToRed(sumRx);
+		var colorTx = getGreenToRed(sumTx);
+		dataLinks.links[i].colorRx=colorRx;
+		dataLinks.links[i].colorTx=colorTx;
+
+		//alert("RECIBIDO AQUI: "+dataLinks.links[i].colorRx+" TRANSM AQUI: "+dataLinks.links[i].colorTx);
+
+	}
+
+
+
+	for (var i=0;i<dataLinks.links.length;i++){
 		for(var j=0;j<nodes.length;j++){
 			for(var k=0;k<nodes[j].ports.length;k++){
-				if(rawLinks[i].srcPortId == nodes[j].ports[k].portId){
+				if(dataLinks.links[i].srcPortId == nodes[j].ports[k].portId){
 					src = j;
 				}
-				if(rawLinks[i].dstPortId == nodes[j].ports[k].portId){
+				if(dataLinks.links[i].dstPortId == nodes[j].ports[k].portId){
 					trg = j;
 				}
 			}
 			if (!((src==null)||(trg==null))){
+
 				//alert(src+" "+trg);
-				links.push({"source":src,"target":trg,"value":8});
+
+
+				links.push({"source":src,"target":trg,"value":8,"type":"suit","color":dataLinks.links[i].colorTx});
+				links.push({"source":trg,"target":src,"value":8,"type":"suit","color":dataLinks.links[i].colorRx});
 				src= null;
 				trg=null;
 			}
 		}	
+	}
+
+
+	for (var i=0; i<dataTerminals.terminals.length; i++){
+		//alert(i);
+		//Dummy request to..
+		var reqUriRx ="/AppServer/webapi/statistics/port/"+dataTerminals.terminals[i].portAPId+"/receiveBytes/AVERAGE/minute";
+		//alert (reqUriRx);
+		//Obtained DUMMYajax response
+		var response1;// = {"idObject":"00:01:d4:ca:6d:c4:44:1e:3","parameter":"receiveBytes","timeAxxis":[1389907545,1389907546,1389907547,1389907548,1389907549,1389907550,1389907551,1389907552,1389907553,1389907554,1389907555,1389907556,1389907557,1389907558,1389907559,1389907560,1389907561,1389907562,1389907563,1389907564,1389907565,1389907566,1389907567,1389907568,1389907569,1389907570,1389907571,1389907572,1389907573,1389907574,1389907575,1389907576,1389907577,1389907578,1389907579,1389907580,1389907581,1389907582,1389907583,1389907584,1389907585,1389907586,1389907587,1389907588,1389907589,1389907590,1389907591,1389907592,1389907593,1389907594,1389907595,1389907596,1389907597,1389907598,1389907599,1389907600,1389907601,1389907602,1389907603,1389907604],"valueAxxis":[0.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,695.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,61.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]};
+		$.ajax({
+			type: "GET",
+			url: reqUriRx,
+			//contentType: 'application/json',
+			//datatype: "application/vmd.dxat.appserver.topology.switches.collection+json",
+//			headers: {
+//			Accept : "application/vmd.dxat.appserver.topology.switches.collection+json"
+//			},
+			success : function(result) {
+				response1 = result;
+			}
+		});	
+		//Dummy request to...
+		var reqUriTx ="/AppServer/webapi/statistics/port/"+dataTerminals.terminals[i].portAPId+"/transmitBytes/AVERAGE/minute";
+		//alert (reqUriTx);
+		//Obtained DUMMYajax response
+		var response2;//={"idObject":"00:01:d4:ca:6d:c4:44:1e:3","parameter":"transmitBytes","timeAxxis":[1389908038,1389908039,1389908040,1389908041,1389908042,1389908043,1389908044,1389908045,1389908046,1389908047,1389908048,1389908049,1389908050,1389908051,1389908052,1389908053,1389908054,1389908055,1389908056,1389908057,1389908058,1389908059,1389908060,1389908061,1389908062,1389908063,1389908064,1389908065,1389908066,1389908067,1389908068,1389908069,1389908070,1389908071,1389908072,1389908073,1389908074,1389908075,1389908076,1389908077,1389908078,1389908079,1389908080,1389908081,1389908082,1389908083,1389908084,1389908085,1389908086,1389908087,1389908088,1389908089,1389908090,1389908091,1389908092,1389908093,1389908094,1389908095,1389908096,1389908097],"valueAxxis":[12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0,12500000.0]};
+		$.ajax({
+			type: "GET",
+			url: reqUriTx,
+			//contentType: 'application/json',
+			//datatype: "application/vmd.dxat.appserver.topology.switches.collection+json",
+//			headers: {
+//			Accept : "application/vmd.dxat.appserver.topology.switches.collection+json"
+//			},
+			success : function(result) {
+				response2 = result;
+			}
+		});	//Transmitted bytes
+		//Recieved bytes
+		var sumRx=0;
+		var sumTx=0;
+
+		for (var j=0; j<response1.valueAxxis.length;j++){
+			sumRx+=response1.valueAxxis[j];
+			sumTx+=response2.valueAxxis[j];
+		}
+
+		sumRx = ((sumRx/60)*8/1000000).toFixed(2);
+		sumTx = ((sumTx/60)*8/1000000).toFixed(2);
+		//alert("REC: "+sumRx+" TRANSM: "+sumTx);
+		var colorRx = getGreenToRed(sumRx);
+		var colorTx = getGreenToRed(sumTx);
+		dataTerminals.terminals[i].colorRx=colorRx;
+		dataTerminals.terminals[i].colorTx=colorTx;
 	}
 
 	for (var i=0; i<terminals.length;i++){
@@ -110,60 +239,105 @@ function createTopologyGraph(){
 						//alert("STEP2: "+nodes[j].ports[k].portId);
 						if (nodes[i].portAPId==nodes[j].ports[k].portId){
 							src = i;
-							trg =j;
-
+							trg =j;	
 							//alert("Link Pushed: "+nodes[j].swId+ " TO "+nodes[i].terminalId);
-							links.push({"source":src,"target":trg,"value":8});
+							links.push({"source":src,"target":trg,"value":8,"type":"suit","color":nodes[i].colorRx});
+							links.push({"source":trg,"target":src,"value":8,"type":"suit","color":nodes[i].colorTx});
 						}
 					}
 				}
 			}
 		}	
-	} 
+	}
 
-	var width = 400, height = 300;
+	function getGreenToRed(percent){
+		g = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+		r = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+		return 'rgb('+r+','+g+',0)';
+	}
+
+
+}//END GET TOPO WEATHER MAP
+
+
+function createTopologyGraph(){
+
+	//Represenació grafic 
+	var width = 400,
+	height = 300
+
+	var force = d3.layout.force()
+	.nodes(nodes)
+	//.gravity(.05)
+	.links(links)
+	.size([width, height])
+	.linkDistance(110)
+	.charge(-1000)
+	.on("tick", tick)
+	.start();
+
+	/*var force = d3.layout.force()
+    .gravity(.05)
+    .distance(100)
+    .charge(-1000)
+    .linkDistance(80)
+    .size([width, height]);
+
+  force
+      .nodes(nodes)
+      .links(links)
+      .start();*/
 
 	var svg = d3.select("#topo").append("svg")
 	.attr("width", "100%")
-	.attr("height", height)
-	.attr("viewBox", "0 0 " + width + " " + height )
-	.attr("preserveAspectRatio", "xMidYMid meet")
-	.attr("pointer-events", "all")
-	.call(d3.behavior.zoom().on("zoom", redraw));
+	.attr("height", height);
 
-	var pict = svg
-	.append('svg:g');
 
-	function redraw() {
-		pict.attr("transform",
-				"translate(" + d3.event.translate + ")"
-				+ " scale(" + d3.event.scale + ")");
-	};
+	svg.append("defs").selectAll("marker")
+	.data(["suit", "licensing", "resolved"])
+	.enter().append("marker")
+	.attr("id", function(d) {return d; })
+	.attr("viewBox", "0 -5 10 10")
+	.attr("refX", 29)
+	.attr("refY", -3)
+	.attr("markerWidth", 8)
+	.attr("markerHeight", 8)
+	.attr("orient", "auto")
+	.append("path")
+	.attr("d", "M0,-5L10,0L0,5");
 
-	var force = d3.layout.force()
-	.gravity(.05)
-	.distance(100)
-	.charge(-1000)
-	.linkDistance(80)
-	.size([width, height]);
-
-	force
-	.nodes(nodes)
-	.links(links)
-	.start();
-
-	var link = svg.selectAll(".link")
+	var path = svg.append("g").selectAll("path")
 	.data(links)
-	.enter().append("line")
-	.attr("class", "link")
-	//.style("stroke", "#F00") Amb aixo es canvia el color del link
-	.style("stroke-width", function(d) { return Math.sqrt(d.value); });
+	.enter().append("path")
+	.attr("class", function(d) { return "link " + d.type; })
+	.attr("marker-end", function(d) { return "url(#" + d.type + ")"; })
+	.style("stroke",  function(d) {return d.color;/*	var color="#000";
+    								switch (d.color){
+    									case 2:
+    										return d.color;
+    									case 10:
+    										return color = "#F00";
+        								default:
+            								var color="#00F";
+        								return color;
+    									}*/}); //Amb aixó canviarem el color!
+
+
+	/* var link = svg.selectAll(".link")
+     .data(links)
+     .enter().append("line")
+     .attr("class", "link")
+     //.style("stroke", "#F00") //Amb aixo es canvia el color del link
+     .style("stroke-width", function(d) { return Math.sqrt(d.value); });*/
 
 	var node = 	svg.selectAll(".node")
 	.data(nodes)
 	.enter().append("g")
 	.attr("class", "node")
+	.on("tick",  function (d){return path.attr("d", linkArc)})
 	.call(force.drag);
+
+
 
 	node.append("title").text(function(d) {
 		if(d.portAPId ==null){
@@ -180,9 +354,9 @@ function createTopologyGraph(){
 
 	node.append("image")
 	.attr("xlink:href", function (d)	{	if (d.portAPId ==null){
-		return "/AppServer/img/switch.svg"
+		return "/img/switch.svg"
 	}else{
-		return "/AppServer/img/terminal.svg"
+		return "/img/terminal.svg"
 	}
 	})
 	.attr("x", function (d)	{	if (d.portAPId ==null){
@@ -210,15 +384,38 @@ function createTopologyGraph(){
 	}
 	});
 
-	force.on("tick", function() {
+	/*force.on("tick", function() {
 		link.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });
+		    .attr("y1", function(d) { return d.source.y; })
+		    .attr("x2", function(d) { return d.target.x; })
+		    .attr("y2", function(d) { return d.target.y; });
+	    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });	
+		});*/
+	function tick() {
+		path.attr("d", linkArc);
+		node.attr("transform", transform);
+		// text.attr("transform", transform);
+	}
 
-		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });	
-	});
+	function linkArc(d) {
+		var dx = d.target.x - d.source.x,
+		dy = d.target.y - d.source.y,
+		dr = Math.sqrt(dx * dx + dy * dy);
+		return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+	}
+
+	function transform(d) {
+		return "translate(" + d.x + "," + d.y + ")";
+	}
+
+
+
+	//TODO SET INTERVAL WEATHER MAP HERE
 	
+	refreshIntervalTWM = setInterval(function() {
+		getTopoWeatherMap();
+	},60000);
+
 	node.on("click", function(d) {
 		StopSwitchStats();
 
